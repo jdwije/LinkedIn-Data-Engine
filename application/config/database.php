98012821818cddebebf6bcc543344a778f5021fe
Jason Wijegooneratne <service@jwije.com>
2013-05-22 12:00:35 +1000

    updates to config

--- application/config/database.php
+++ application/config/database.php
@@ -49,8 +49,8 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'root';
-$db['default']['password'] = 'Arz1|9KaF6[yg!6';
+$db['default']['username'] = 'philiapp';
+$db['default']['password'] = 'RSpjwdV5ZtTMee4m';
 $db['default']['database'] = 'lde';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
--- server.conf.md
+++ server.conf.md
@@ -4,3 +4,7 @@
 user : root
 password : Arz1|9KaF6[yg!6
 
+## mysql application
+user: philiapp
+passowrd: RSpjwdV5ZtTMee4m
+
