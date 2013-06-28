12351b697e2003712da91feab8b89e6ff6041d62
Jason Wijegooneratne <service@jwije.com>
2013-06-28 14:53:57 +1000

    cahnge text

--- application/views/pages/welcome.php
+++ application/views/pages/welcome.php
@@ -1,13 +1,13 @@
 <!-- Our prominent "take the test now" headliner -->
 <div class='row-fluid'>
 	<p>This app was designed to help Philip Schneider complete his Master Thesis reseaerch on entrepreneurship.</p>
-	<p>Participating is easy! To join, click the large <i>Participate</i> button above and login with your LinkedIn Account.</p>
+	<p>Participating is easy! To join, click the large <i>Participate</i> button below and login with your LinkedIn Account.</p>
 	<p>All data will be anonymised and used strictly for research purposes! We will not sell, market, or distribute your data to any 3rd parties period.</p>
 	<p>
 		In order to fetch your data from LinkedIn we need you to authorize this app with your LinkedIn account. To do this click the <i>Participate</i> button 
-		above, you will be presented with a login form by LinkedIn. Once you complete and submit this form, the app will gather your data and notify you on completion.
+		below, you will be presented with a login form by LinkedIn. Once you complete and submit this form, the app will gather your data and notify you on completion.
 	</p>
-		
+
 	 <a class="btn btn-primary btn-large" href="<?php echo site_url('participate'); ?>">
       Participate!
     </a>
